59e736dbe8a0a870c042c5d55cc120d33c7a66e2
Deivecito <agustingonzalez200133@gmail.com>
2023-08-17 18:57:33 -0300

    logout

--- js/index.js
+++ js/index.js
@@ -5,6 +5,18 @@ setTimeout(() => {
 }, 50000);
 
 document.addEventListener("DOMContentLoaded", function(){
+    validar_login();
+
+ document.getElementById("logout").addEventListener("click", function(){
+    localStorage.clear();
+    sessionStorage.clear();
+    document.getElementById("alert-logout").classList.add('show');
+    setTimeout(() => {
+        document.getElementById("alert-logout").classList.remove('show');
+        location.href="login.html"
+    }, 2500);
+ });
+
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
@@ -17,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+<<<<<<< Updated upstream
         document.getElementById("btn-login").addEventListener("click", function() {
   if ((localStorage.getItem("email") !== null) && (localStorage.getItem("pass") !== null)){
     document.getElementById("alert-yeslogin").classList.add('show');
@@ -25,10 +38,30 @@ document.addEventListener("DOMContentLoaded", function(){
     }, 2500);
 }else {
    document.getElementById("alert-nologin").classList.add('show');
+=======
+
+    function validar_login() {
+        //Si la key email está vacia es porque no se inició sesión previamente.
+        if (localStorage.getItem("email") === null) {
+            document.getElementById("alert-nologin").classList.add('show');
+>>>>>>> Stashed changes
     setTimeout(() => {
         document.getElementById("alert-nologin").classList.remove('show');
         location.href="login.html"
     }, 2000);
+<<<<<<< Updated upstream
 };
         });
 });
+=======
+        } else {
+            document.getElementById("alert-yeslogin").classList.add('show');
+    setTimeout(() => {
+        document.getElementById("alert-yeslogin").classList.remove('show');
+    }, 2500);
+        };
+    };
+});
+
+
+>>>>>>> Stashed changes
