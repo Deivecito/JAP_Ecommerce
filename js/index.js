092a8e829c8945f3402e031bf6a17bd8049b71de
Deivecito <agustingonzalez200133@gmail.com>
2023-08-17 11:36:09 -0300

    AlertStorage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+
+setTimeout(() => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("pass");
+}, 50000);
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
@@ -11,4 +17,18 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+        document.getElementById("btn-login").addEventListener("click", function() {
+  if ((localStorage.getItem("email") !== null) && (localStorage.getItem("pass") !== null)){
+    document.getElementById("alert-yeslogin").classList.add('show');
+    setTimeout(() => {
+        document.getElementById("alert-yeslogin").classList.remove('show');
+    }, 2500);
+}else {
+   document.getElementById("alert-nologin").classList.add('show');
+    setTimeout(() => {
+        document.getElementById("alert-nologin").classList.remove('show');
+        location.href="login.html"
+    }, 2000);
+};
+        });
 });
--- js/login.js
+++ js/login.js
@@ -1,14 +1,27 @@
-document.getElementById("alert-login").classList.remove('show');
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById('inicio').addEventListener('click', function(){
+    document.getElementById('inicio').addEventListener('click', function(event){
         event.preventDefault();
         document.getElementById("alert-login").classList.remove("show");
     
-if (document.getElementById('InputEmail1').value !== "" &&
-    document.getElementById('InputPassword1').value !== "") {
+if ((document.getElementById('InputEmail1').value !== "") &&
+    (document.getElementById('InputPassword1').value !== "") &&
+    (document.getElementById("checkbox-login").checked)) {
+        localStorage.setItem("email", document.getElementById('InputEmail1').value)
+        localStorage.setItem("pass", document.getElementById('InputPassword1').value)
     location.href="index.html";
-    }else
+} if ((document.getElementById('InputEmail1').value !== "") &&
+(document.getElementById('InputPassword1').value !== "") &&
+!(document.getElementById("checkbox-login").checked)) {
+    location.href="index.html";
+
+}
+
+
+
+
+
+else
     document.getElementById("alert-login").classList.add('show');
     setTimeout(() => { 
         document.getElementById("alert-login").classList.remove('show');
